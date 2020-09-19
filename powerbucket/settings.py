2951b8c7983f1975e278ff3f51c9f446916bbf0e
jabbate7 <joseph.abbate7@gmail.com>
2020-09-19 13:32:43 -0400

    Heroku not deploying because of problem with static files

--- powerbucket/settings.py
+++ powerbucket/settings.py
@@ -137,4 +137,4 @@ STATIC_URL = '/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
